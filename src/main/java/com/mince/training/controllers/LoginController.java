fd4037bd00c47fa015c6b0059e8b3f77abe03c35
Pavel Belyaev <beljaev.pavel.kr@gmail.com>
2016-10-07 19:50:32 +0200

    add localization

--- src/main/java/com/mince/training/controllers/LoginController.java
+++ src/main/java/com/mince/training/controllers/LoginController.java
@@ -3,45 +3,53 @@ package com.mince.training.controllers;
 import com.mince.training.objects.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Locale;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class LoginController {
-	
+
+    @Autowired
+    private MessageSource messageSource;
+
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView main(@ModelAttribute User user, HttpSession session) {
+	public ModelAndView main(@ModelAttribute User user, HttpSession session, Locale locale) {
+        System.out.println(locale.getDisplayLanguage());
+        System.out.println(messageSource.getMessage("locale",new String[] {locale.getDisplayName(locale)}, locale));
         user.setName("usernamevalue");
 		return new ModelAndView("login", "user", new User());
 	}
 
 	@RequestMapping(value = "/check-user", method = RequestMethod.POST)
-	public String checkUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model, ModelMap modelMap) {
-        System.out.println(model);
-        System.out.println(modelMap);
+	public ModelAndView checkUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Locale locale) {
 
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("locale", messageSource.getMessage("locale", new String[] { locale.getDisplayName(locale) }, locale));
         if (bindingResult.hasErrors()) {
-            return "login";
+            modelAndView.setViewName("login");
+        } else {
+            modelAndView.setViewName("main");
         }
 
-        return "main";
+        return modelAndView;
 	}
 
 	@RequestMapping(value = "/failed", method = RequestMethod.GET)
